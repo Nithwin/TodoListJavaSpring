a863b0945281ad6cb5ece0fb8493d14bb315d5be
Nithwin <vmnithwin@gmail.com>
2025-09-08 14:10:33 +0530

    Project Started

---
+++ src/test/java/com/nithwin/todolistjavaspring/TodoListJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nithwin.todolistjavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListJavaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
